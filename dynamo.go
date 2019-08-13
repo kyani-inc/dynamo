db832ea37f1c2e0e737b500bbd410de2ea4789c5
catmullet <catmulltrim@gmail.com>
2019-08-13 08:21:58 -0600

    Adding the goods

---
+++ dynamo.go
@@ -0,0 +1,122 @@
+package dynamo
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
+
+var DB *dynamodb.DynamoDB
+
+func init() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	DB = dynamodb.New(sess)
+}
+
+func Delete(tableName, key, value string) (err error) {
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			key: {
+				S: aws.String(value),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+
+	_, err = DB.DeleteItem(input)
+
+	return
+}
+
+func Put(tableName string, item interface{}, ttl string) (err error) {
+	i, err := dynamodbattribute.MarshalMap(item)
+
+	input := &dynamodb.PutItemInput{
+		Item:      i,
+		TableName: aws.String(tableName),
+	}
+
+	if ttl != "" {
+		input.Item["ttl"] = &dynamodb.AttributeValue{N: aws.String(ttl)}
+	}
+
+	_, err = DB.PutItem(input)
+	return
+}
+
+func GetItem(tableName string, keys map[string]string, object interface{}) (err error) {
+
+	builder := expression.NewBuilder()
+
+	filter := expression.ConditionBuilder{}
+	firstKey := true
+
+	for key, value := range keys {
+		if firstKey {
+			filter = expression.Name(key).Equal(expression.Value(value))
+			firstKey = false
+		} else {
+			filter = filter.And(expression.Name(key).Equal(expression.Value(value)))
+		}
+	}
+
+	exp := builder.WithFilter(filter)
+
+	expr, err := exp.Build()
+
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		ProjectionExpression:      expr.Projection(),
+		TableName:                 aws.String(tableName),
+	}
+
+	out, err := DB.Scan(params)
+
+	if len(out.Items) > 0 {
+		dynamodbattribute.UnmarshalMap(out.Items[0], &object)
+	}
+
+	return
+}
+
+func GetItemList(tableName string, keys map[string]string, object interface{}) (err error) {
+
+	builder := expression.NewBuilder()
+
+	filter := expression.ConditionBuilder{}
+	firstKey := true
+
+	for key, value := range keys {
+		if firstKey {
+			filter = expression.Name(key).Equal(expression.Value(value))
+			firstKey = false
+		} else {
+			filter = filter.And(expression.Name(key).Equal(expression.Value(value)))
+		}
+	}
+
+	exp := builder.WithFilter(filter)
+
+	expr, err := exp.Build()
+
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		ProjectionExpression:      expr.Projection(),
+		TableName:                 aws.String(tableName),
+	}
+
+	out, err := DB.Scan(params)
+
+	err = dynamodbattribute.UnmarshalListOfMaps(out.Items, &object)
+
+	return
+}
